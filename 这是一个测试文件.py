293d3799fbe6146f0e5a1836e8cb245ae15fd30d
李波 <libo_sober@163.com>
2020-08-27 21:17:29 +0800

    这是一个测试文件

---
+++ 这是一个测试文件.py
@@ -0,0 +1,4 @@
+print("csdn博客：https://blog.csdn.net/qq_31910669")
+print("博客园：https://www.cnblogs.com/SoberLee/")
+print("码云：https://gitee.com/libo-sober")
+print("Github：https://github.com/libo-sober")
